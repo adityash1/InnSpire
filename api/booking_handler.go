0ef36a18c38d2d5fab8663af00e17ae892cf7784
adityash1 <aditya167411@gmail.com>
2023-08-30 14:43:34 +0530

    feat: user can only see his bookings for specific room

--- api/booking_handler.go
+++ api/booking_handler.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"github.com/adityash1/go-reservation-api/db"
+	"github.com/adityash1/go-reservation-api/types"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
 )
 
 type BookingHandler struct {
@@ -16,7 +18,6 @@ func NewBookHandler(store *db.Store) *BookingHandler {
 	}
 }
 
-// TODO: this needs to be admin authorized!
 func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
 	bookings, err := h.store.Booking.GetBookings(c.Context(), bson.M{})
 	if err != nil {
@@ -25,12 +26,21 @@ func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
 	return c.JSON(bookings)
 }
 
-// TODO: this needs to be user authorized
 func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
 	id := c.Params("id")
 	booking, err := h.store.Booking.GetBookingByID(c.Context(), id)
 	if err != nil {
 		return err
+	}
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return err
+	}
+	if booking.UserID != user.ID {
+		return c.Status(http.StatusUnauthorized).JSON(genericResp{
+			Type: "error",
+			Msg:  "unauthorized",
+		})
 	}
 	return c.JSON(booking)
 }
